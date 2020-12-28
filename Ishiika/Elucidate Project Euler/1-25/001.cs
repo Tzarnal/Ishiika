7f95f7b69932ff02ab0af1c35e8c5b49340221a4
Stefan van Oudenaarden <s.oudenaarden@gmail.com>
2020-12-28 16:40:26 +0100

    Project Euler: Problem 1

---
+++ Ishiika/Elucidate Project Euler/1-25/001.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Collections.Generic;
+using RegExtract;
+using Serilog;
+using Ishiika;
+
+namespace Problem_001
+{
+    internal class Problem_001 : IIshiikaProblem
+    {
+        public string ProblemName => "Multiples of 3 and 5";
+        public string ProblemURL => "https://projecteuler.net/problem=1";
+        public int ProblemID => 1;
+
+        public void Solve()
+        {
+            var rangerLowerBound = 0;
+            var rangerUpperBound = 1000;
+            var firstrequirement = 3;
+            var secondRequirement = 5;
+
+            var awnser = Enumerable.Range(rangerLowerBound, rangerUpperBound)
+                    .Where(n => n % firstrequirement == 0 || n % secondRequirement == 0)
+                    .Sum(n => n);
+
+            Log.Verbose("Test");
+
+            Log.Information("The sum of all numbers from {rangeLowerBound} to {rangerUpperBound} divisable by {firstrequirement} or {secondRequirement} is {awnser}",
+                rangerLowerBound, rangerUpperBound, firstrequirement, secondRequirement, awnser);
+        }
+    }
+}
---
+++ Ishiika/Elucidate Project Euler/Program.cs
@@ -0,0 +1,13 @@
+﻿using Ishiika.Framework;
+
+namespace Elucidate_Project_Euler
+{
+    internal static class Program
+    {
+        private static void Main(string[] args)
+        {
+            var cli = new CLI();
+            cli.Process(args);
+        }
+    }
+}
