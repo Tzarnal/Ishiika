4d9be5a72c520d17d5d6805293b14d1fe3dacdbe
Stefan van Oudenaarden <s.oudenaarden@gmail.com>
2020-12-30 13:54:57 +0100

    Project Euler: 10

---
+++ Ishiika/Elucidate Project Euler/1-25/010.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using Serilog;
+using Ishiika;
+using Ishiika.Library;
+
+namespace Problems1_25
+{
+    public class Problem010 : IIshiikaProblem
+    {
+        public string ProblemName => "Summation of primes";
+        public string ProblemURL => $"https://projecteuler.net/problem={ProblemID}";
+        public int ProblemID => 10;
+
+        public void Solve()
+        {
+            SumOfPrimes(2_000_000);
+        }
+
+        private void SumOfPrimes(int upperLimit)
+        {
+            var primes = IshiikaMath.GeneratePrimes(upperLimit);
+            long sum = 0;
+
+            foreach (var number in primes)
+            {
+                sum += number;
+            }
+
+            Log.Information("The sum of the first {upperLimit} primes is {sum}", upperLimit, sum);
+        }
+    }
+}
